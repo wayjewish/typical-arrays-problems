ae0af70131a7e46e654ff8d80bbccfd07260d103
Dmitry Uvarov <diman_niks@mail.ru>
2021-01-19 11:39:35 +0300

    feat: implement min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+
+  let sum = array.reduce((sum, current) => sum + current);
+  return sum / array.length;
 }
